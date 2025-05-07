a63bd412400bf17f77243edd531803ceed3542da
tykyphatky <gg.fctaiphat@yahoo.com>
2025-05-07 23:33:42 +0700

    Update MyDoctor.tsx

--- src/Pages/MyDoctor/MyDoctor.tsx
+++ src/Pages/MyDoctor/MyDoctor.tsx
@@ -507,8 +507,7 @@ const MyDoctorProfilePage: React.FC = () => {
               )}
             </>
           )}
-
-          
+       
           {!hasProfileData && (
             <Card 
               style={{ 
